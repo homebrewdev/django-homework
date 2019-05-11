b8392f254d2bacc7dd77d503ad27e29271b4a570
homebrewdev <xstall@yandex.ru>
2019-05-11 23:32:17 +0400

    + добавлена обработка методом POST в форме Spend (добавление нового расхода) + сделано новое представление spends-list.html выводит весь список расходов

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from blog import views
+import blog.views
 
 urlpatterns = [
     path('blog/', views.post_list, name = 'post_list'),
     path('', views.post_list, name = 'post_list'),
-    path('spend/', views.spend_url, name = 'add_spend'),
+    path('add_spend/', views.add_spend, name = 'add_spend'),
+    path('spends-view/', views.spends_view_url, name='view_spends'),
+    path('spends-list/', blog.views.ListSpendView.as_view(), name='blog/spends-list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,65 @@
 from django.shortcuts import render
 from django.utils import timezone
 from blog.models import Post
+from blog.models import Spend
+from django.http import HttpResponseRedirect
+from django.views.generic import ListView
+from django.views.generic import CreateView
+
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-def spend_url(request):
-    return render(request, 'blog/spend.html', {})
+
+def add_spend(request):
+    if request.method == "POST":
+        s = Spend()
+        s.category = request.POST.get("category")
+        s.name = request.POST.get("name")
+        s.created_date = timezone.now()
+        s.amount = request.POST.get("amount")
+        s.save()
+    return render(request, 'blog/add_spend.html')
+
+
+def spends_view_url(request):
+    header = "Таблица затрат:"
+    spend_category = Spend.category
+    spend_name = Spend.name
+    spend_amount = Spend.amount
+
+    data = {"header": header,
+            "category": spend_category,
+            "name": spend_name,
+            "amount": spend_amount}
+
+    return render(request, 'blog/spend-view.html', context=data)
+
+
+class ListSpendView(ListView):
+    model = Spend
+    template_name = 'blog/spends-list.html'
+
+
+class CreateSpendView(CreateView):
+    model = Spend
+    template_name = 'add_spend.html'
+    fields = ['category', 'name', 'amount']
+
+    def get_success_url(self):
+        return HttpResponseRedirect("/")
+
+"""
+# сохранение данных в бд
+def add_spend(request):
+    if request.method == "POST":
+        s = Spend()
+        s.category = request.POST.get("category")
+        s.name = request.POST.get("name")
+        s.created_date = '1 января'
+        s.amount = request.POST.get("amount")
+        s.save()
+    return HttpResponseRedirect("/")
+"""
